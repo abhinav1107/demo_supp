100298ddd13bfa81f43a977040b814c78fdaeebd
Abhinav Kumar <abhinav1107@yahoo.co.in>
2018-05-21 03:30:01 +0530

    Create group_function1.py

---
+++ group_function1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+def function1():
+    """This is a demo function 1"""
+    print("I am in demo function1")
+
+
+def function2():
+    """This is a demo function 2"""
+    print("I am in demo function2")
